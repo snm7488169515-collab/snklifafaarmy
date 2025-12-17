ac572d19141f8d9afce2bb560ad9f893bdaad41a
snm7488169515-collab <snm7488169515@gmail.com>
2025-12-17 18:34:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 const path = require('path');
 require('dotenv').config();
 const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-
 const app = express();
 app.use(bodyParser.json());
 app.use(session({
@@ -42,7 +39,8 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
 });
+
 
